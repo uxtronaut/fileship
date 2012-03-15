62e18d876b5b72131acc720fc553342f21318342
Zach Payne <zach.payne@oregonstate.edu>
2012-03-15 11:29:13 -0700

    set preferred sass syntax

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Fileship
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.sass.preferred_syntax = :sass
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
