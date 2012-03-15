48b0558cd77245f622824e0cda92002857c26e96
Zach Payne <zach.payne@oregonstate.edu>
2012-03-15 11:17:50 -0700

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileship::Application.config.secret_token = '904a26c44ca86327c55d7773124aa411e5fc183fc4f45d823a19f17df92410fe1dd2936f5d00874b222725d0cfcb82611c790442555b792a4011e1ee4250ca59'
