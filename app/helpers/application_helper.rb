4404d693d39315552568566e53407ed72a1674ae
Zach Payne <zach.payne@oregonstate.edu>
2012-03-15 12:07:37 -0700

    add tophat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def tophat_tag
+    link_to (image_tag 'http://oregonstate.edu/osuhomepage/regions/top-hat/1.2/images/osu-tag.gif', {
+      :alt => 'Oregon State University',
+      :class => 'tag'
+    }), 'http://oregonstate.edu/', {
+      :title => 'Oregon State University',
+      :class => 'brand'
+    }
+  end
+
 end
