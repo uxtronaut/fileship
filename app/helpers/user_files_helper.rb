4e1d4f8d3ca9311d6ca1b08fa66a904bb80693e0
Zach Payne <uxtronaut@gmail.com>
2012-03-21 10:27:04 -0700

    initial work on dropdown actions

--- app/helpers/user_files_helper.rb
+++ app/helpers/user_files_helper.rb
@@ -1,2 +1,18 @@
 module UserFilesHelper
+
+  def sharing_button(file)
+    button_text = "#{content_tag :i, '', :class => 'icon-share icon-large'} Sharing"
+    link_to button_text.html_safe, "#sharing-modal-#{file.id}", :data => {:toggle => 'modal'}
+  end
+
+  def password_button(file)
+    button_text = "#{content_tag :i, '', :class => 'icon-lock icon-large'} Password"
+    link_to button_text.html_safe, "#password-modal-#{file.id}", :data => {:toggle => 'modal'}
+  end
+
+  def edit_file_button(file)
+    button_text = "#{content_tag :i, '', :class => 'icon-edit icon-large'} Edit"
+    link_to button_text.html_safe, "#edit-file-modal-#{file.id}", :data => {:toggle => 'modal'}
+  end
+
 end
