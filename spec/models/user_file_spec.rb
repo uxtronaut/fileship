28fc45ca4bef7d33d42fe221925b56e6ba373315
Michael Woffendin <woffendm@onid.orst.edu>
2012-11-06 12:27:40 -0800

    Change days_until_purge to be easier to read
    Re: #21686
    
    * Change days_until_purge to use the .days method instead of multiplying by the number of second in a day
    * Add unit tests for purge_old_files

--- spec/models/user_file_spec.rb
+++ spec/models/user_file_spec.rb
@@ -15,10 +15,36 @@ describe UserFile do
     end
   end
 
+
   describe "#extension" do
     it "returns the file's extension" do
       user_file = FactoryGirl.create(:user_file)
       user_file.extension.should == 'png'
+    end
+  end
+
+
+  describe "#purge_old_files" do
+    it "should remove all files older than the pre-defined number of days" do
+      Fileship::Application.config.fileship_config['days_until_purge'] = 1
+      # creates user_file one day before the purge date. Should be purged
+      user_file = FactoryGirl.create(:user_file, :created_at => (Date.today.to_time_in_current_zone - (UserFile.days_until_purge + 1).days) )
+      UserFile.all.should_not be_empty
+      UserFile.where("created_at < ?", UserFile.purge_date).should_not be_empty
+      UserFile.purge_old_files
+      UserFile.all.should be_empty
+    end
+    
+    it "should not remove files that aren't older than the pre-defined number of days" do
+      # creates user_file right now. Should not be purged.
+      user_file = FactoryGirl.create(:user_file)
+      # creates user_file on the purge date. Should not be purged.
+      user_file_2 = FactoryGirl.create(:user_file, :created_at => (Date.today.to_time_in_current_zone - UserFile.days_until_purge.days) )
+      UserFile.all.should_not be_empty
+      UserFile.where("created_at < ?", UserFile.purge_date).should be_empty
+      UserFile.purge_old_files
+      UserFile.all.include?(user_file).should eq true
+      UserFile.all.include?(user_file_2).should eq true
     end
   end
 end
