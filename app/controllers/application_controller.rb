5cbf9948e02874eb80a34c17793f457331e065e2
Michael Woffendin <woffendm@onid.orst.edu>
2013-06-19 11:29:16 -0700

    Add example stylesheet
    * Remove OSU use policy
    * Allow for inclusion of external stylesheet
    * Require login to submit feedback
    * Fix style issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :load_settings
   before_filter :get_current_user
-
   helper_method :current_user, :signed_in?
 
+  
+  # Loads settings specified in app.yml
+  def load_settings
+    settings = Fileship::Application.config.fileship_config
+    @policy_path = settings['policy_path']
+    @stylesheet_path = settings['stylesheet_path']
+    @logo_image_path = settings['logo_image_path']
+    @logo_url = settings['logo_url']
+  end
+  
+
+  # Retrieves current user if they are in the application, or attempts to add them using information
+  # from ldap. 
   def get_current_user
     if session[:cas_user]
       uid = session[:cas_user]
-
       @current_user = User.find_or_import(uid)
-
       # Ensure the user was successfully retrieved from ldap. If no information for them is
       # available, the app is unusable, so we need to let them try to log in again
       unless @current_user
@@ -21,10 +32,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+  # Destroys current user's session, redirects to CAS login page.
   def signout
     RubyCAS::Filter.logout(self, welcome_url)
   end
 
+
+  # Renders permission denied page
   def render_403
     respond_to do |format|
       format.html { render :file => Rails.root.join('public', '403.html'), :status => :forbidden, :layout => 'dark' }
@@ -32,10 +47,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+  # Returns current user
   def current_user
     return @current_user
   end
 
+
+  # Returns whether a user is signed in
   def signed_in?
     !!current_user
   end
