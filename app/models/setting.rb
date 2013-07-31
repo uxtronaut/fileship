d27834583129c3e85b665aa86ce0e392357cfa92
Michael Woffendin <woffendm@onid.orst.edu>
2013-07-31 14:13:05 -0700

    Finalize file log ui
    * Add app setting for "days until log purge"
    * Fix css problems with modals
    * Add validation to ensure that logs aren't deleted before their files
    * Update name of "Days until purge" to "days until file purge"
    * Fix tests
    * Add tooltips where appropriate
    * Update cron script to purge old logs

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,9 +5,12 @@ class Setting < ActiveRecord::Base
 
   # Validates the current app setting based on its code.
   def validate_all_settings
-    if name == "Days until purge"
+    if name.index("purge")
       unless (value.to_f > 0) && (value.to_f % 1 == 0)
         errors.add(:value, "Not a positive integer")
+      end
+      if name.index("log") && (value.to_f < Setting.find_by_name("Days until file purge").value.to_f)
+        errors.add(:value, "Must be greater than 'Days until file purge'")
       end
     else
       unless value.blank? || value.index("http")
