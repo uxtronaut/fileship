f9323ebd3df7ab2a318c7fcef0969a74590b3372
Michael Woffendin <woffendm@onid.orst.edu>
2013-08-02 13:35:52 -0700

    Fix empty array error thing for file logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -70,33 +70,33 @@ class ApplicationController < ActionController::Base
 
 # Error handling
 
-  # Rescues internal server error with 500
-  rescue_from Exception do |exception|
-    respond_to do |format|
-      format.html { render "pages/500.html.erb", :status => :internal_server_error, :layout => 'dark' }
-      format.json { render :json => {:error => 'Error 500, error...'}, :status => :rejected, :content_type => 'text/plain' }
-    end
-  end
-  
-  
-  # Rescues from invalid record with 404
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    respond_to do |format|
-      format.html { render "pages/404.html.erb", :status => :not_found, :layout => 'dark' }
-      format.json { render :json => {:error => 'Error 404, not found...'}, :status => :not_found, :content_type => 'text/plain' }
-    end
-  end
-  
-  
-  # Rescues with non-existing page with 404
-  rescue_from ActionController::RoutingError do |exception|
-    respond_to do |format|
-      format.html { render "pages/404.html.erb", :status => :not_found, :layout => 'dark' }
-      format.json { render :json => {:error => 'Error 404, not found...'}, :status => :not_found, :content_type => 'text/plain' }
-    end
-  end
- 
- 
+#  # Rescues internal server error with 500
+#  rescue_from Exception do |exception|
+#    respond_to do |format|
+#      format.html { render "pages/500.html.erb", :status => :internal_server_error, :layout => 'dark' }
+#      format.json { render :json => {:error => 'Error 500, error...'}, :status => :rejected, :content_type => 'text/plain' }
+#    end
+#  end
+#  
+#  
+#  # Rescues from invalid record with 404
+#  rescue_from ActiveRecord::RecordNotFound do |exception|
+#    respond_to do |format|
+#      format.html { render "pages/404.html.erb", :status => :not_found, :layout => 'dark' }
+#      format.json { render :json => {:error => 'Error 404, not found...'}, :status => :not_found, :content_type => 'text/plain' }
+#    end
+#  end
+#  
+#  
+#  # Rescues with non-existing page with 404
+#  rescue_from ActionController::RoutingError do |exception|
+#    respond_to do |format|
+#      format.html { render "pages/404.html.erb", :status => :not_found, :layout => 'dark' }
+#      format.json { render :json => {:error => 'Error 404, not found...'}, :status => :not_found, :content_type => 'text/plain' }
+#    end
+#  end
+#  
+#  
   # Renders permission denied page
   def render_403
     respond_to do |format|
@@ -105,13 +105,13 @@ class ApplicationController < ActionController::Base
     end
   end
   
- 
-  # Rescues unprocessible entity with 422
-  rescue_from ActiveResource::ClientError do |exception|
-    respond_to do |format|
-      format.html { render "pages/422.html.erb", :status => :rejected, :layout => 'dark' }
-      format.json { render :json => {:error => 'Error 404, rejected...'}, :status => :rejected, :content_type => 'text/plain' }
-    end
-  end
+#  
+#  # Rescues unprocessible entity with 422
+#  rescue_from ActiveResource::ClientError do |exception|
+#    respond_to do |format|
+#      format.html { render "pages/422.html.erb", :status => :rejected, :layout => 'dark' }
+#      format.json { render :json => {:error => 'Error 404, rejected...'}, :status => :rejected, :content_type => 'text/plain' }
+#    end
+#  end
   
 end
