d6f9fe5a3a827bcf506e786a2ca3e0dfd822655d
Nivens, Cody R <cody.nivens@oregonstate.edu>
2013-08-08 09:55:46 -0700

    Added from puppet

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Fileship::Application.config.secret_token = '904a26c44ca86327c55d7773124aa411e5fc183fc4f45d823a19f17df92410fe1dd2936f5d00874b222725d0cfcb82611c790442555b792a4011e1ee4250ca59'
+#
+# # Your secret key for verifying the integrity of signed cookies.
+# # If you change this key, all old signed cookies will become invalid!
+# # Make sure the secret is at least 30 characters and all random,
+# # no regular words or you'll be exposed to dictionary attacks.
+Fileship::Application.config.secret_token = '571369efc8d2bc44da91be5e2a7c724ad66488d5e96c0e76ca0196b635ac9fb5d77fa701cf5e980fa675897520c325dbdd11fbc61cf2192f1765dcda28e0057c'
