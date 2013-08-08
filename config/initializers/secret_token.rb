fb6e866c6dedd314866b28ac2ed32121093b3107
Nivens, Cody R <cody.nivens@oregonstate.edu>
2013-08-08 09:56:20 -0700

    Removed token from repo

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-#
-# # Your secret key for verifying the integrity of signed cookies.
-# # If you change this key, all old signed cookies will become invalid!
-# # Make sure the secret is at least 30 characters and all random,
-# # no regular words or you'll be exposed to dictionary attacks.
-Fileship::Application.config.secret_token = '571369efc8d2bc44da91be5e2a7c724ad66488d5e96c0e76ca0196b635ac9fb5d77fa701cf5e980fa675897520c325dbdd11fbc61cf2192f1765dcda28e0057c'
