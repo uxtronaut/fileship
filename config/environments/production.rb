1efeaec1db6e194591153cbc465985c9854b29b3
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2013-08-27 10:33:38 -0700

    Disable officer button when nothing selected
    * Hide top nav bar on welcome screen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Fileship::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( bootstrap.js detect_browser.js notices.js tooltip.js alerts.css bootstrap.css fileship.css)
+  config.assets.precompile += %w( bootstrap.js detect_browser.js notices.js tooltip.js users.js alerts.css bootstrap.css fileship.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
